426cfdc1f9312d1728631d15fc04786f9eabb2b2
Leonid Petrov1 <leonid_petrov1@epam.com>
2021-06-06 19:48:14 +0400

    Add sqlite db

---
+++ src/Database/ConnectionString.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace SimpleOnlineShop.Database
+{
+    public class ConnectionString
+    {
+        public static readonly string Value
+            = "Data Source=" + Path.Combine(Directory.GetCurrentDirectory(), "database", "simple-online-shop.sqlite");
+    }
+}
---
+++ src/Database/database.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE IF NOT EXISTS Products(
+    ProductId INTEGER PRIMARY KEY AUTOINCREMENT
+    ,ProductName TEXT NOT NULL
+    ,UnitPrice DECIMAL(10, 2) NOT NULL
+    ,Quantity INTEGER NOT NULL
+);
+
+INSERT INTO Products(ProductName, UnitPrice, Quantity)
+SELECT 'Icecream', 2.20, 100
+WHERE NOT EXISTS (SELECT 1 FROM Products WHERE ProductId = 1)
